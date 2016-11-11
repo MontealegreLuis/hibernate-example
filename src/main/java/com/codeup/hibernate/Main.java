aecf6406cae656f02c19fb94c18ba470a9e9a97d
Luis Montealegre <montealegreluis@gmail.com>
2016-11-10 22:30:45 -0600

    Query categories table.

--- src/main/java/com/codeup/hibernate/Main.java
+++ src/main/java/com/codeup/hibernate/Main.java
@@ -9,9 +9,22 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.query.Query;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        Session session = openSession();
+        session.save(Category.named("action"));
+        Query query = session.createQuery("FROM Category");
+        List<Category> categories = query.list();
+        System.out.println(Arrays.toString(categories.toArray()));
+        session.close();
+    }
+
+    private static Session openSession() {
         StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
             .configure()
             .build()
@@ -19,8 +32,6 @@ public class Main {
 
         SessionFactory sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
 
-        Session session = sessionFactory.openSession();
-        session.save(Category.named("action"));
-        session.close();
+        return sessionFactory.openSession();
     }
 }
